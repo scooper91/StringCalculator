c12688fdb7d2a92104992f1d8cb1b31f2ef6ba1b
Sophie Cooper <sophie.cooper@7digital.com>
2014-10-01 12:59:05 +0100

    Can add strings of numbers

---
+++ StringCalculator.Tests/Calculator.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator.Tests
+{
+	public class Calculator
+	{
+		public IEnumerable<int> StringToNumbers;
+		public int AddNumbers;
+
+		public int Total()
+		{
+			return AddNumbers;
+		}
+
+		public void Add(string numbers)
+		{
+			StringToNumbers = numbers.Split(',').Select(int.Parse);
+			foreach (var stringToNumber in StringToNumbers)
+			{
+				AddNumbers += stringToNumber;
+			}
+		}
+	}
+}
---
+++ StringCalculator.Tests/Tests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+	public class StringCalculatorTests
+	{
+		[TestCase("0", 0)]
+		[TestCase("1", 1)]
+		[TestCase("2", 2)]
+		public void Should_Return_Single_Number_When_Single_Number_Entered(
+			string number, int expectedOutcome)
+		{
+			var calculator = new Calculator();
+			calculator.Add(number);
+
+			Assert.That(calculator.Total(), Is.EqualTo(expectedOutcome));
+		}
+
+		[Test]
+		public void Should_Return_2_When_11_Entered()
+		{
+			var calculator = new Calculator();
+			calculator.Add("1,1");
+
+			Assert.That(calculator.Total(), Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Should_Return_5_When_Numbers_Entered()
+		{
+			var calculator = new Calculator();
+			calculator.Add("1,0,2,2");
+
+			Assert.That(calculator.Total(), Is.EqualTo(5));
+		}
+	}
+}
